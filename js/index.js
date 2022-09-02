b6eea7a61ae38d194c371815c192300f11b8b173
kipkemoi98 <gilbert.kipkemoi@student.moringaschool.com>
2022-09-02 10:40:24 +0300

    add js functions

--- js/index.js
+++ js/index.js
@@ -36,48 +36,54 @@ const search = () => {
             }
         }
     }
-
-
-    function searchEUKOS() {
-        const form = document.getElementById("search-form")
-
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
-            const search = event.target['search-input'].value;
-            event.target.reset();
-        })
-    }
-    function postUserInfo() {
-        const form = document.getElementById("user-address");
-        form.addEventListener('submit', (event) => {
-            event.preventDefault()
-
-            const userName = event.target['user-name'].value;
-            const userEmail = event.target['user-email'].value;
-            console.log(userName, userEmail);
-
-            const userInfo = {
-                userName: userName,
-                userEmail: userEmail
-            };
-
-            fetch('http://localhost:3000/UserData', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                    Accept: 'application/json'
-                },
-                body: JSON.stringify(userInfo)
-            })
-                .then((response) => response.json())
-                .then((result) => {
-                    alert(`${result.userName}, your data has been saved successfully!`)
-                })
-
-            event.target.reset()
-        })
-    }
-    showlogin.addEventListener("click", () => {
-        popuplogin.style.display = "block";
-    });
 }
+
+function searchEUKOS() {
+    const form = document.getElementById("search-form")
+
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+        const search = event.target['search-input'].value;
+        event.target.reset();
+    })
+}
+function postUserInfo() {
+    const form = document.getElementById("user-address");
+    form.addEventListener('submit', (event) => {
+        event.preventDefault()
+
+        const userName = event.target['user-name'].value;
+        const userEmail = event.target['user-email'].value;
+        console.log(userName, userEmail);
+
+        const userInfo = {
+            userName: userName,
+            userEmail: userEmail
+        };
+        const username = document.querySelector("#useremail");
+        const password = document.querySelector("password");
+        const submit = document.querySelector("submit")
+
+        submit.addEventListener("click", (event))=>{
+            event.preventDefault();
+        }
+
+        fetch('http://localhost:3000/UserData', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json'
+            },
+            body: JSON.stringify(userInfo)
+        })
+            .then((response) => response.json())
+            .then((result) => {
+                alert(`${result.userName}, your data has been saved successfully!`)
+            })
+
+        event.target.reset()
+    })
+}
+showlogin.addEventListener("click", () => {
+    popuplogin.style.display = "block";
+});
